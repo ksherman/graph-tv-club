eacab17ba48f34e5f78f219db3fc5a32581e863d
Kevin Sherman <kevin@kshermphoto.com>
2018-01-23 10:06:44 -0600

    Start show detail sidebars

---
+++ src/components/ui/ShowDetailSidebar.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="sidebar-wrapper left-side">
+    <div class="sidebar-header">
+      Close
+    </div>
+    <div class="sidebar-content">
+      Lots of Show Details!
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ShowDetailSidebar',
+};
+</script>
