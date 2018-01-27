d3d3a8a993817415e96df000e065e29ae6bb067a
Kevin Sherman <kevin@kshermphoto.com>
2018-01-27 10:23:41 -0600

    Clean up Show Details view and refined chart options

--- src/components/ShowDetails.vue
+++ src/components/ShowDetails.vue
@@ -1,9 +1,39 @@
 <template>
-  <div class="show-wrapper">
-    <pre>{{ getShowDetails }}</pre>
-    <template v-if="getShowDataForScatterplot.length === Number(getShowDetails.totalSeasons)">
-      <show-scatter-plot :episodeDataset="getShowDataForScatterplot" />
-    </template>
+  <div class="show-wrapper" v-if="getShowDetails.Title">
+    <h2 class="show-title">
+      {{ getShowDetails.Title }}
+      <span class="year">{{ getShowDetails.Year }}</span>
+    </h2>
+    <div class="show-details">
+      <div class="poster">
+        <img :src="getShowDetails.Poster" />
+      </div>
+      <div class="information">
+        <div class="information-line">
+          <strong>Plot:</strong> {{ getShowDetails.Plot }}
+        </div>
+        <div class="information-line">
+          <strong>Typical runtime:</strong> {{ getShowDetails.Runtime }}
+        </div>
+        <div class="information-line">
+          <strong>Overall score:</strong>
+          {{ getShowDetails.Ratings[0].Value }}
+          <span class="text-medium">
+            {{ getShowDetails.Ratings[0].Source }} from {{ getShowDetails.imdbVotes }} votes
+          </span>
+        </div>
+      </div>
+    </div>
+    <h2>Season Ratings:</h2>
+    <div class="show-season-charts" v-if="getShowDataForScatterplot.length === Number(getShowDetails.totalSeasons)">
+      <div
+        class="season-chart"
+        :key="`${getShowDetails.imdbID}-season-${dataset.label}`"
+        v-for="dataset in getShowDataForScatterplot"
+      >
+        <show-scatter-plot :episodeDataset="dataset" />
+      </div>
+    </div>
   </div>
 </template>
 
@@ -17,13 +47,21 @@ export default {
     ShowScatterPlot,
   },
   created() {
-    this.runShowDetailFetch(this.$route.params.showId);
+    if (!this.getShowDetails.Title) {
+      this.fetchData();
+    }
   },
   computed: {
     ...mapGetters(['getShowDetails', 'getShowSeasons', 'getShowDataForScatterplot']),
   },
   methods: {
     ...mapActions(['runShowDetailFetch']),
+    fetchData() {
+      this.runShowDetailFetch(this.$route.params.showId);
+    },
+  },
+  watch: {
+    $route: 'fetchData',
   },
 };
 </script>
--- src/components/ShowScatterPlot.vue
+++ src/components/ShowScatterPlot.vue
@@ -9,12 +9,47 @@ export default {
     Scatter,
   },
   mounted() {
-    this.renderChart({
-      datasets: this.episodeDataset,
-      options: {
-        responsive: true,
+    this.renderChart(
+      {
+        datasets: [this.episodeDataset],
       },
-    });
+      {
+        responsive: true,
+        legend: { display: false },
+        layout: {
+          padding: 10,
+        },
+        title: {
+          display: true,
+          position: 'bottom',
+          text: this.episodeDataset.label,
+        },
+        scales: {
+          xAxes: [
+            {
+              display: false,
+              ticks: {
+                max: this.episodeDataset.data.length + 1,
+              },
+            },
+          ],
+          yAxes: [
+            {
+              display: true,
+              scaleLabel: {
+                display: true,
+                labelString: 'Score',
+              },
+              ticks: {
+                beginAtZero: true,
+                max: 10,
+                stepSize: 1,
+              },
+            },
+          ],
+        },
+      },
+    );
   },
 };
 </script>
